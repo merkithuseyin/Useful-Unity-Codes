99355271ca85f840ee562962bc3daab093ecb00b
Hüseyin Merkit <merkithuseyin@gmail.com>
2020-02-24 15:35:48 +0300

    Added save systems (PlayerPrefs and JSON)

---
+++ SaveSystem-JSON.cs
@@ -0,0 +1,50 @@
+﻿// saves/loads data from previously created GameManager singleton to/from JSON formatted file
+
+using System;
+using System.IO;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    private static string path = Path.Combine(Application.persistentDataPath, "data.json");
+    public static void SaveGameData()
+    {
+        JsonData data = new JsonData();
+        data.GameManagerToJsonDataClass();
+        File.WriteAllText(path, JsonUtility.ToJson(data, true));
+    }
+
+    public static void LoadGameData()
+    {
+        if (File.Exists(path))
+        {
+            JsonData data = JsonUtility.FromJson<JsonData>(File.ReadAllText(path));
+            data.JsonDataClassToGameManager();
+        }
+        else
+        {
+            Debug.LogError("No file found in " + path);
+        }
+    }
+
+    [Serializable]
+    private class JsonData
+    {
+        public int myInt;
+        public DateTime myDateTime;
+
+        public void GameManagerToJsonDataClass()
+        {
+            GameManager gm = GameManager.Instance;
+            myInt = gm.myInt;
+            myDateTime = gm.currentTime;
+        }
+
+        public void JsonDataClassToGameManager()
+        {
+            GameManager gm = GameManager.Instance;
+            gm.myInt = myInt;
+            gm.currentTime = myDateTime;
+        }
+    }
+}
---
+++ SaveSystem-PlayerPrefs.cs
@@ -0,0 +1,21 @@
+﻿// saves/loads data from previously created GameManager singleton
+// https://docs.unity3d.com/ScriptReference/PlayerPrefs.html
+
+using System;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static void SaveGameData()
+    {
+        PlayerPrefs.SetInt("MyInt", GameManager.Instance.myInt);
+        PlayerPrefs.SetString("DateTime", GameManager.Instance.myDateTime.ToString("s")); //e.g. 2008-10-01T17:04:32
+        PlayerPrefs.Save();
+    }
+
+    public static void LoadGameData()
+    {
+        GameManager.Instance.level = PlayerPrefs.GetInt("MyInt");
+        GameManager.Instance.myDateTime = DateTime.ParseExact(PlayerPrefs.GetString("DateTime"), "s", System.Globalization.CultureInfo.InvariantCulture);
+    }
+}
