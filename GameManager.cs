db65499358b8611060d19ae5e1ad1b476c489949
Hüseyin Merkit <merkithuseyin@gmail.com>
2020-02-25 16:39:36 +0300

    Added gamemanager singleton file

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance 
+    { 
+        get 
+        { 
+            return _instance; 
+        } 
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
